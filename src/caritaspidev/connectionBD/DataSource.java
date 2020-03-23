75e54acfd0af5dd2f80d4d74345be071cc57e69a
Jawadions <jawadi.ons.97@gmail.com>
2020-03-24 00:35:52 +0100

    fix structure

--- src/caritaspidev/connectionBD/DataSource.java
+++ src/caritaspidev/connectionBD/DataSource.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package connectionBD;
+package caritaspidev.connectionBD;
 
 
 import java.sql.Connection;
