5ca9b40475423051d5ea75552333964657dd62cf
Jawadions <jawadi.ons.97@gmail.com>
2020-03-25 17:02:05 +0100

    fix sturcture2

---
+++ src/caritaspidev/entityPublicite/publicite.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caritaspidev.entityPublicite;
+
+import java.util.Objects;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author asus
+ */
+public class publicite {
+    
+    private int id;
+    private String image ;
+    private String description ;
+    private boolean etat;
+    private int like ;
+    private ImageView photo;
+
+    public publicite(int id, String image, String description, boolean etat, int like, ImageView photo) {
+        this.id = id;
+        this.image = image;
+        this.description = description;
+        this.etat = etat;
+        this.like = like;
+        this.photo = photo;
+    }
+
+    public publicite(int id, String image, String description, boolean etat, int like) {
+        this.id = id;
+        this.image = image;
+        this.description = description;
+        this.etat = etat;
+        this.like = like;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 29 * hash + this.id;
+        hash = 29 * hash + Objects.hashCode(this.image);
+        hash = 29 * hash + Objects.hashCode(this.description);
+        hash = 29 * hash + (this.etat ? 1 : 0);
+        hash = 29 * hash + this.like;
+        hash = 29 * hash + Objects.hashCode(this.photo);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "publicite{" + "id=" + id + ", image=" + image + ", description=" + description + ", etat=" + etat + ", like=" + like + ", photo=" + photo + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final publicite other = (publicite) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.image, other.image)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (this.etat != other.etat) {
+            return false;
+        }
+        if (this.like != other.like) {
+            return false;
+        }
+        if (!Objects.equals(this.photo, other.photo)) {
+            return false;
+        }
+        return true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEtat() {
+        return etat;
+    }
+
+    public void setEtat(boolean etat) {
+        this.etat = etat;
+    }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public ImageView getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(ImageView photo) {
+        this.photo = photo;
+    }
+
+    public publicite(String image, String description, boolean etat, int like, ImageView photo) {
+        this.image = image;
+        this.description = description;
+        this.etat = etat;
+        this.like = like;
+        this.photo = photo;
+    }
+
+    public publicite(String image, String description, boolean etat, int like) {
+        this.image = image;
+        this.description = description;
+        this.etat = etat;
+        this.like = like;
+    }
+
+   
+
+}  
+  
---
+++ src/caritaspidev/services/ServicePublicite.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caritaspidev.services;
+
+import caritaspidev.connectionBD.DataSource;
+import caritaspidev.entityPublicite.publicite;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author asus
+ */
+public class ServicePublicite implements Iservice<publicite>{
+    private Connection con;
+    private Statement ste;
+    private PreparedStatement pre;
+   
+    
+    public ServicePublicite(){
+    con = DataSource.getInstance().getConnection();
+    }
+
+    @Override
+    public void ajouter(publicite t) throws SQLException {
+      
+        pre=con.prepareStatement("INSERT INTO `caritass`.`publicite` ( `image`, `description`, `etat`, `like`) VALUES ( ?, ?, ?, ?);");
+    pre.setString(1, t.getImage());
+    pre.setString(2, t.getDescription());
+    
+    pre.setInt(4, t.getLike());
+    pre.setBoolean(3, t.isEtat());
+    pre.executeUpdate();
+   
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException {
+   if(chercher(id)){
+            System.out.println("exist");
+        pre=con.prepareStatement("delete from `caritass`.`publicite` where id  = (?);");
+        pre.setInt(1,id);
+            System.out.println(pre.execute());
+       return true;}
+        else 
+        {System.out.println("nexiste pas");
+            return false;}
+    }
+
+    @Override
+    public boolean chercher(int id) throws SQLException {
+        String req="select * from publicite";
+        List<Integer> list = new ArrayList<>();
+        
+        try {
+            ste=con.createStatement();
+            ResultSet rs = ste.executeQuery(req);
+            while(rs.next()){
+                list.add(rs.getInt(1));
+                
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ServicePublicite.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+        return list.contains(id);
+    }
+
+    @Override
+    public boolean chercher_ajout(publicite t) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int update(publicite t, int id) throws SQLException {
+         if(chercher(id)){
+        
+        pre=con.prepareStatement("UPDATE publicite SET  image = ? , description = ?  , etat =? , like =? WHERE id = ?");
+         
+    pre.setString(1, t.getImage());
+    
+    
+    pre.setString(2,t.getDescription());
+  
+    pre.setInt(4, t.getLike());
+    pre.setBoolean(3, t.isEtat());
+    pre.setInt(7,id);
+    pre.executeUpdate();
+    return 1;}
+        return 0;
+    }
+
+    @Override
+    public List<publicite> readAll() throws SQLException {
+       
+     String req="select * from publicite  ";
+        List<publicite> list = new ArrayList<>();
+        
+        try {
+            ste=con.createStatement();
+            ResultSet rs = ste.executeQuery(req);
+            while(rs.next()){
+                
+               
+               
+                ImageView v = new ImageView();
+                
+                v.setImage(new Image(rs.getString(3)));
+                v.setFitWidth(100);
+                v.setFitHeight(100);
+               
+                //ystem.out.println(v.getImage().toString());
+                publicite p2=new publicite(rs.getInt(1),rs.getString(2),rs.getString(3), rs.getBoolean(4),rs.getInt(5));
+                
+                p2.setPhoto(v);
+                list.add(p2);
+              // list.add(new Publicite(rs.getInt(1),rs.getString(2),rs.getString(3),d1, rs.getString(6), d2,rs.getDouble(7)));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ServicePublicite.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list  ;
+    }}
